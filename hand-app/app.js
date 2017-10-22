47c80840a244fcedf7e7dd927a09737b4a08b56c
Maria Romanova <aemulare@users.noreply.github.com>
2017-10-22 01:19:17 -0400

    Adds console message regarding app sever port.

--- hand-app/app.js
+++ hand-app/app.js
@@ -1,10 +1,13 @@
 const express = require('express');
+
 const app = express();
 
 
 const PORT = process.env.PORT || 3000;
 app.use((req, res) => {
-    res.end(`Server is up and running on port ${PORT}`);
+  res.end('Welcome to HAND Project!');
 });
 
-app.listen(PORT);
+app.listen(PORT, () => {
+  console.log(`Server is up and running on port ${PORT}`);
+});
