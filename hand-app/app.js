cc0611bc594b5c3d99ce7d1ded0b0dcceb281d71
Maria Romanova <aemulare@users.noreply.github.com>
2017-10-22 22:37:51 -0400

    Minor bug fix in app.js.

--- hand-app/app.js
+++ hand-app/app.js
@@ -9,9 +9,5 @@ app.use((req, res) => {
 });
 
 app.listen(PORT, () => {
-  newFunction()(`Server is up and running on port ${PORT}`);
+  console.log(`Server is up and running on port ${PORT}`);
 });
-function newFunction() {
-    return console.log;
-}
-
