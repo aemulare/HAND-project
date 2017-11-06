ccfb65ff1b0bf83a6012dba7f0af46c102c55474
Maria Romanova <aemulare@users.noreply.github.com>
2017-11-05 19:35:06 -0500

    Added dev dependencies. Installed SASS. Defines statis assets path.

--- hand-app/app.js
+++ hand-app/app.js
@@ -1,12 +1,19 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
 
 
 const PORT = process.env.PORT || 3000;
-app.use((req, res) => {
-  res.end('Welcome to HAND Project!');
+app.use(express.static(path.join(__dirname, 'public')));
+app.set('views', path.resolve(__dirname, 'views/sign-up'));
+app.set('view engine', 'ejs');
+
+
+app.get('/signup', (req, res) => {
+  res.render('sign-up');
 });
+
 
 app.listen(PORT, () => {
   console.log(`Server is up and running on port ${PORT}`);
