d15010d48a6093d5ad5f38e06d5100267524db7d
Maria Romanova <aemulare@users.noreply.github.com>
2017-11-05 19:39:26 -0500

    Configured webpack.

---
+++ hand-app/webpack.config.js
@@ -0,0 +1,43 @@
+/*
+  Webpack configuration file
+*/
+
+const path = require('path');
+const webpack = require('webpack');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+module.exports = {
+  entry: './assets/js/application',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'public'),
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ExtractTextPlugin.extract({ fallback: 'style-loader', use: 'css-loader' }),
+      },
+      {
+        test: /\.scss$/,
+        use: ExtractTextPlugin.extract({
+          use: [{ loader: 'css-loader' }, { loader: 'sass-loader' }],
+          fallback: 'style-loader',
+        }),
+      },
+      { test: /\.(png|jpg|jpeg|gif|cur)$/, use: ['file-loader'] },
+      { test: /\.(eot|svg|ttf|woff(2)?)(\?v=\d+\.\d+\.\d+)?/, use: ['url-loader'] },
+    ],
+  },
+
+  devtool: 'eval-source-map',
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js', '.jsx'],
+  },
+
+  plugins: [
+    new webpack.ProvidePlugin({ $: 'jquery', jQuery: 'jquery' }),
+    new ExtractTextPlugin('bundle.css'),
+  ],
+};
