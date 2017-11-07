6ad6ab16088270c7de1c87174be57ad577973285
Maria Romanova <aemulare@users.noreply.github.com>
2017-11-06 22:20:01 -0500

    Renames db.json to database.json

--- hand-app/app/models/index.js
+++ hand-app/app/models/index.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const path = require('path');
 const Sequelize = require('sequelize');
-const config = require('../../config/db.json');
+const config = require('../../config/database.json');
 
 const db = {};
 const sequelize = new Sequelize(config.database, config.username, config.password, config);
