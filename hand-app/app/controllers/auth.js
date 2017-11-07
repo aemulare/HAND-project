28a0b3c2d859f58a38d13d4648bf28fb1c19a8e9
Maria Romanova <aemulare@users.noreply.github.com>
2017-11-06 22:42:47 -0500

    Creates sign up POST action.

--- hand-app/app/controllers/auth.js
+++ hand-app/app/controllers/auth.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const db = require('../models');
 
 const router = express.Router();
 module.exports = (app) => {
@@ -14,3 +15,23 @@ router.get('/signup', (req, res) => {
 router.get('/signin', (req, res) => {
   res.render('sign_in');
 });
+
+
+router.post('/signup', (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+  const passwordConfirmation = req.body.password_confirm;
+
+  db.users.create({
+    email,
+    passwordHash: 'kvakvakva',
+  })
+    .then(() => {
+      res.redirect('/');
+    })
+    .catch((err) => {
+      console.log(err);
+      res.redirect('/signup');
+    });
+});
+
--- hand-app/app/models/index.js
+++ hand-app/app/models/index.js
@@ -1,7 +1,9 @@
 const fs = require('fs');
 const path = require('path');
 const Sequelize = require('sequelize');
-const config = require('../../config/database.json');
+
+const env = process.env.NODE_ENV || 'development';
+const config = require('../../config/database.json')[env];
 
 const db = {};
 const sequelize = new Sequelize(config.database, config.username, config.password, config);
