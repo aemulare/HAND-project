5e63616d392418cc1b52baaceb5d04a29f4368f9
Maria Romanova <aemulare@users.noreply.github.com>
2017-11-14 22:05:45 -0500

    Designs header react component.

--- hand-app/app/components/Header.jsx
+++ hand-app/app/components/Header.jsx
@@ -1,52 +1,24 @@
-
 import React from 'react';
+import '../assets/styles/header.css';
 
-
-export const Header = () =>
-  (<nav className="navbar navbar-default no-margin">
-    {}
+export const Header = () => (
+  <nav className="navbar navbar-default no-margin">
     <div className="navbar-header fixed-brand">
-      <button
-        type="button"
-        className="navbar-toggle collapsed"
-        data-toggle="collapse"
-        id="menu-toggle"
-      >
-        <span className="glyphicon glyphicon-th-large" aria-hidden="true" />
-      </button>
-      <a className="navbar-brand" href="#">
-        {}
-        <img
-          src="img/hand_logo_rectangle.svg"
-          alt="HAND logo"
-          height="65px"
-          width="auto"
-        />
+      <a className="navbar-brand">
+        <img src="img/hand_logo_rectangle.svg" alt="HAND logo" height="65px" width="auto" />
       </a>
     </div>
-    {}
-    <div
-      className="collapse navbar-collapse"
-      id="bs-example-navbar-collapse-1"
-    >
+
+    <div className="collapse navbar-collapse">
       <ul className="nav navbar-nav">
         <li className="active">
-          <button
-            className="navbar-toggle collapse in"
-            data-toggle="collapse"
-            id="menu-toggle-2"
-          >
-            <span
-              className="glyphicon glyphicon-th-large"
-              aria-hidden="true"
-            />
+          <button className="navbar-toggle collapse in" data-toggle="collapse">
+            <span className="glyphicon glyphicon-th-large" />
           </button>
         </li>
       </ul>
     </div>
-    {}
-   </nav>
-  );
-
+  </nav>
+);
 
 export default Header;
--- hand-app/app/containers/app.jsx
+++ hand-app/app/containers/app.jsx
@@ -1,19 +1,11 @@
 import React, { Component } from 'react';
-import logo from '../assets/images/logo.svg';
-import '../assets/styles/app.css';
-
+import Header from '../components/header';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">HAND - Help After Natural Disasters</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <Header />
       </div>
     );
   }
