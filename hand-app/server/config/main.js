9028a95ce5b67f4bb0d31f241957ea10fe6b2a94
Maria Romanova <aemulare@users.noreply.github.com>
2017-11-19 14:31:38 -0500

    Moves server side modules to the appropriate package.json.

--- hand-app/server/config/main.js
+++ hand-app/server/config/main.js
@@ -1,3 +1,24 @@
-module.exports = {
-  port: process.env.PORT || 8000
+const path = require('path');
+
+const rootPath = path.normalize(`${__dirname}/..`);
+const env = process.env.NODE_ENV || 'development';
+
+const config = {
+  development: {
+    root: rootPath,
+    app: { name: 'hand-app' },
+    port: process.env.PORT || 8000
+  },
+  test: {
+    root: rootPath,
+    app: { name: 'hand-app' },
+    port: process.env.PORT || 8000
+  },
+  production: {
+    root: rootPath,
+    app: { name: 'hand-app' },
+    port: process.env.PORT || 8000
+  }
 };
+
+module.exports = config[env];
