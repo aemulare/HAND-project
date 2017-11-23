1cb0d3f1f5b708d6cf5933f508410f3aef215c7e
Maria Romanova <aemulare@users.noreply.github.com>
2017-11-23 18:46:47 -0500

    Fixes menu and menu-item components.

--- hand-app/client/app/components/main/menu_item.jsx
+++ hand-app/client/app/components/main/menu_item.jsx
@@ -1,18 +1,30 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import '../../assets/styles/app.css';
 
-const MenuItem = () => {
-  const Icon = this.props.icon;
+const MenuItem = ({ label, icon, active }) => {
+  const Icon = icon;
+
   return (
-    <li className={this.props.active ? 'active' : null}>
+    <li className={active ? 'active' : null}>
       <a>
         <span className="pull-left">
           <Icon size={24} />
         </span>
-        {this.props.label}
+        {label}
       </a>
     </li>
   );
+};
+
+MenuItem.propTypes = {
+  label: PropTypes.string.isRequired,
+  icon: PropTypes.func.isRequired,
+  active: PropTypes.bool
+};
+
+MenuItem.defaultProps = {
+  active: false
 };
 
 export default MenuItem;
