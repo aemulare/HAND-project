b791c66b2ddeb1b650444a0520b0ca322bd14e47
Maria Romanova <aemulare@users.noreply.github.com>
2017-11-23 18:32:04 -0500

    Enables Cross-Origin Resource Sharing (CORS) on the server side.

--- hand-app/server/config/middleware.js
+++ hand-app/server/config/middleware.js
@@ -8,4 +8,16 @@ module.exports = (app) => {
   // Body parser middleware
   app.use(bodyParser.urlencoded({ extended: false }));
   app.use(bodyParser.json());
+
+  // Enable CORS from client-side
+  // Cross-Origin Resource Sharing (CORS)
+  // https://developer.mozilla.org/en-US/docs/Web/HTTP/CORS
+  app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization, Access-Control-Allow-Credentials');
+    res.header('Access-Control-Allow-Credentials', 'true');
+    next();
+  });
+
 };
