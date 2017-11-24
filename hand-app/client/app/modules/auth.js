f4bd480722f03b59544194cf1dd4528143f9acde
Maria Romanova <aemulare@users.noreply.github.com>
2017-11-24 11:40:09 -0500

    Creates Auth helper class used for authentication/authorization purposes.

---
+++ hand-app/client/app/modules/auth.js
@@ -0,0 +1,42 @@
+class Auth {
+  /**
+   * Authenticates a user
+   * (saves a token in the local storage).
+   * @param {string} token
+   */
+  static login(token) {
+    localStorage.setItem('auth_token', token);
+  }
+
+
+  /**
+   * Deauthenticates a user
+   * (removes a token from the local storage).
+   */
+  static logout() {
+    localStorage.removeItem('auth_token');
+  }
+
+
+  /**
+   * Checks if a uses is authenticated
+   * (checks if a token is saved in the local storage).
+   *
+   * @returns {boolean}
+   */
+  static isAuthenticated() {
+    return token() !== null;
+  }
+
+
+  /**
+   * Gets a token from the local storage.
+   *
+   * @returns {string}
+   */
+  static token() {
+    return localStorage.getItem('auth_token');
+  }
+}
+
+export default Auth;
