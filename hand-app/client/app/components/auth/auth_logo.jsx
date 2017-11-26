c7acbeeeea018d65f22862d8912baef14f2b42ad
Maria Romanova <aemulare@users.noreply.github.com>
2017-11-25 23:32:27 -0500

    Converts styles for welcome header components via CSS modules.

--- hand-app/client/app/components/auth/auth_logo.jsx
+++ hand-app/client/app/components/auth/auth_logo.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { Image } from 'react-bootstrap';
+import css from './styles/auth.scss';
 
 const AuthLogo = () => (
   <div>
     <Link to="/">
-      <Image src="img/hand-logo.png" className="center-block" responsive alt="hand-logo" id="hand-logo" />
+      <Image src="img/hand-logo.png" className={`center-block ${css.hand_logo}`} responsive alt="hand-logo" />
     </Link>
   </div>
 );
--- hand-app/client/app/components/welcome/welcome_header.jsx
+++ hand-app/client/app/components/welcome/welcome_header.jsx
@@ -1,18 +1,18 @@
 import React from 'react';
 import { Navbar, Nav, NavItem } from 'react-bootstrap';
 import { LinkContainer } from 'react-router-bootstrap';
-import '../../assets/styles/welcome_header.scss';
+import css from './styles/header.scss';
 
 const WelcomeHeader = () => (
-  <Navbar collapseOnSelect className="nav-welcome">
-    <Navbar.Header className="header-welcome">
+  <Navbar collapseOnSelect className={css.nav}>
+    <Navbar.Header className={css.header}>
       <Navbar.Brand>
         <p>Help After Natural Disasters</p>
         <img src="img/hand_logo_rectangle_white.svg" alt="HAND logo" height="65px" />
       </Navbar.Brand>
-      <Navbar.Toggle className="toggle-welcome" />
+      <Navbar.Toggle className={css.toggle} />
     </Navbar.Header>
-    <Navbar.Collapse className="collapse-welcome">
+    <Navbar.Collapse className={css.collapse}>
       <Nav pullRight>
         <LinkContainer to="/map" >
           <NavItem eventKey={1}>Welcome</NavItem>
@@ -24,11 +24,12 @@ const WelcomeHeader = () => (
           <NavItem eventKey={3}>Contact</NavItem>
         </LinkContainer>
         <LinkContainer to="/signin" >
-          <NavItem eventKey={4} className="btn btn-default btn-sm signin">Sign In</NavItem>
+          <NavItem eventKey={4} className={`btn btn-default btn-sm ${css.button}`}>Sign In</NavItem>
         </LinkContainer>
         <LinkContainer to="/signup" >
-          <NavItem eventKey={5} className="btn btn-default btn-sm signup">Sign Up</NavItem>
+          <NavItem eventKey={5} className={`btn btn-default btn-sm ${css.button}`}>Sign Up</NavItem>
         </LinkContainer>
+
       </Nav>
     </Navbar.Collapse>
   </Navbar>
