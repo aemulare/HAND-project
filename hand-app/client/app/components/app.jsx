ac996cf011f9983dab8d033628c7117970357e2c
Maria Romanova <aemulare@users.noreply.github.com>
2017-11-25 23:10:08 -0500

    Fixes typo.

--- hand-app/client/app/components/app.jsx
+++ hand-app/client/app/components/app.jsx
@@ -6,7 +6,7 @@ import SignUp from './auth/sign_up';
 import SignIn from './auth/sign_in';
 import SignOut from './auth/sign_out';
 import Root from './main/root';
-import '../assets/styles/app.css';
+import '../assets/styles/app.scss';
 
 
 const App = () => (
