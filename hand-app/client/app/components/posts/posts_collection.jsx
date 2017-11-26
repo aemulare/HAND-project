3aca8ee6400c4a7447b431a5cc518f78750f711f
Maria Romanova <aemulare@users.noreply.github.com>
2017-11-26 13:42:54 -0500

    Removes react-virtualized. Adds simple table (temporary).

--- hand-app/client/app/components/posts/posts_collection.jsx
+++ hand-app/client/app/components/posts/posts_collection.jsx
@@ -1,15 +1,13 @@
 import React from 'react';
 import { FormGroup, FormControl, Glyphicon, ControlLabel } from 'react-bootstrap';
-import { Column, Table, AutoSizer } from 'react-virtualized';
-// import 'react-virtualized/styles.css'; // only needs to be imported once
+import './styles/table.scss';
 
 // Table data as a array of objects
-const list = [
-  { date: '11/23/2017', location: 'New York, NY', title: 'Help', author: 'John Doe', tags: 'food' },
-  { date: '11/23/2017', location: 'New York, NY', title: 'Help', author: 'Mary Smith', tags: 'clothes' },
-  { date: '10/23/2017', location: 'New York, NY', title: 'Help', author: 'Gary Smith', tags: 'shelter' }
-];
-
+// const list = [
+//   { date: '11/23/2017', location: 'New York, NY', title: 'Help', author: 'John Doe', tags: 'food' },
+//   { date: '11/23/2017', location: 'New York, NY', title: 'Help', author: 'Mary Smith', tags: 'clothes' },
+//   { date: '10/23/2017', location: 'New York, NY', title: 'Help', author: 'Gary Smith', tags: 'shelter' }
+// ];
 
 const PostsCollection = () => (
   <div>
@@ -21,45 +19,22 @@ const PostsCollection = () => (
         <Glyphicon glyph="search" />
       </FormControl.Feedback>
     </FormGroup>
-
-    <AutoSizer>
-      {({ height, width }) => (
-        <Table
-          width={width}
-          height={height}
-          headerHeight={20}
-          rowHeight={30}
-          rowCount={list.length}
-          rowGetter={({ index }) => list[index]}
-        >
-          <Column
-            label="Date"
-            dataKey="date"
-            width={200}
-          />
-          <Column
-            label="Location"
-            dataKey="location"
-            width={200}
-          />
-          <Column
-            label="Title"
-            dataKey="title"
-            width={400}
-          />
-          <Column
-            label="Author"
-            dataKey="author"
-            width={300}
-          />
-          <Column
-            label="Tags"
-            dataKey="tags"
-            width={200}
-          />
-        </Table>
-    )}
-    </AutoSizer>
+    <table>
+      <tr>
+        <th>Date</th>
+        <th>Location</th>
+        <th>Title</th>
+        <th>Author</th>
+        <th>Tags</th>
+      </tr>
+      <tr>
+        <td>11/23/2017</td>
+        <td>New York, NY</td>
+        <td>Help</td>
+        <td>John Doe</td>
+        <td>food</td>
+      </tr>
+    </table>
   </div>
 );
 
