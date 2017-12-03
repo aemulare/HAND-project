aee74015d84554809042187e11221d6a2f04cfd6
Maria Romanova <aemulare@users.noreply.github.com>
2017-12-03 13:45:40 -0500

    Implements posts rendering.

--- hand-app/client/app/components/posts/posts_collection.jsx
+++ hand-app/client/app/components/posts/posts_collection.jsx
@@ -1,41 +1,77 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { FormGroup, FormControl, Glyphicon, ControlLabel } from 'react-bootstrap';
+import axios from 'axios';
+import PostsTable from './posts_table';
+import Auth from '../../modules/auth';
 import './styles/table.scss';
 
 // Table data as a array of objects
-// const list = [
+// const data = [
 //   { date: '11/23/2017', location: 'New York, NY', title: 'Help', author: 'John Doe', tags: 'food' },
 //   { date: '11/23/2017', location: 'New York, NY', title: 'Help', author: 'Mary Smith', tags: 'clothes' },
 //   { date: '10/23/2017', location: 'New York, NY', title: 'Help', author: 'Gary Smith', tags: 'shelter' }
 // ];
 
-const PostsCollection = () => (
-  <div>
-    <h1>Find where you can help</h1>
-    <FormGroup validationState="null">
-      <ControlLabel />
-      <FormControl type="text" placeholder="Search posts" />
-      <FormControl.Feedback>
-        <Glyphicon glyph="search" />
-      </FormControl.Feedback>
-    </FormGroup>
-    <table>
-      <tr>
-        <th>Date</th>
-        <th>Location</th>
-        <th>Title</th>
-        <th>Author</th>
-        <th>Tags</th>
-      </tr>
-      <tr>
-        <td>11/23/2017</td>
-        <td>New York, NY</td>
-        <td>Help</td>
-        <td>John Doe</td>
-        <td>food</td>
-      </tr>
-    </table>
-  </div>
-);
+
+class PostsCollection extends Component {
+  constructor() {
+    super();
+    this.state = {
+      posts: [],
+      search: ''
+    };
+
+    // handlers
+    this.handleSearch = this.handleSearch.bind(this);
+  }
+
+  componentDidMount() {
+    const API_URL = 'http://localhost:8000/api/v1';
+    const API_HEADERS = { 'Content-Type': 'application/json', Authorization: Auth.token() };
+    const client = axios.create({
+      baseURL: API_URL,
+      timeout: 1000,
+      headers: API_HEADERS
+    });
+
+    client.get('posts', { })
+      .then((res) => {
+        if (res.status === 200) {
+          this.setState({ posts: res.data });
+          console.log(res.data);
+        }
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+
+
+  handleSearch(event) {
+    this.setState({ search: event.target.value });
+  }
+
+
+  render() {
+    const {
+      posts,
+      search
+    } = this.state;
+
+    return (
+      <div>
+        <h1>Find where you can help</h1>
+        <FormGroup validationState="null">
+          <ControlLabel />
+          <FormControl componentClass="input" type="text" value={search} placeholder="Search posts" onChange={this.handleSearch} />
+          <FormControl.Feedback>
+            <Glyphicon glyph="search" />
+          </FormControl.Feedback>
+        </FormGroup>
+        <PostsTable data={posts} />
+      </div>
+    );
+  }
+}
 
 export default PostsCollection;
---
+++ hand-app/client/app/components/posts/posts_table.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const moment = require('moment');
+
+
+const TableRow = ({ row }) => (
+
+  <tr>
+    <td key={row.created_at}>{moment(row.created_at).format('MMMM Do YYYY') }</td>
+    <td key={row.title}>{row.title}</td>
+    <td key={row.description}>{row.description}</td>
+    <td key={row.user_id}>{row.user_id}</td>
+  </tr>
+);
+
+const PostsTable = ({ data }) => (
+  <div>
+    <table>
+      <tr>
+        <th>Date</th>
+        <th>Title</th>
+        <th>Description</th>
+        <th>Author</th>
+      </tr>
+      {data.map(row => <TableRow row={row} />)}
+    </table>
+  </div>
+);
+
+
+TableRow.propTypes = {
+  row: PropTypes.object.isRequired
+};
+
+PostsTable.propTypes = {
+  data: PropTypes.array.isRequired
+};
+
+
+export default PostsTable;
